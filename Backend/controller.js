35f5591bf015b29b431caa9fab202a7db553e316
Poorna Malinhara Kurrupuarachi <131478375+Malinhara@users.noreply.github.com>
2024-04-07 11:34:52 +0530

    Update controller.js

--- Backend/controller.js
+++ Backend/controller.js
@@ -6,41 +6,41 @@ const crypto = require('crypto');
 const cors = require('cors');
 const userService = require('./Service/User service');
 const productService = require('./Service/product service');
-// const producService = require('./Service/');
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.json());
 
+
+
+// Routes
+const userRouter = express.Router();
+userRouter.post('/register', userService.registerUser);
+userRouter.post('/login', userService.loginUser);
+userRouter.put('/buyProduct', userService.userBuy);
+userRouter.post('/findproduct', userService.findproduct);
+app.post('/logout', userService.logoutUser);
+app.use('/user', userRouter); // Mount userRouter under '/user' path
+
+const productRouter = express.Router();
+productRouter.post('/addProduct', productService.addProduct);
+productRouter.put('/update/:Id', productService.updateProduct);
+productRouter.delete('/delete/:Id', productService.deleteProduct);
+productRouter.get('/productIds', productService.getAllProducts);
+productRouter.get('/getproduct/:Id', productService.getoneProduct);
+app.use('/product', productRouter); // Mount productRouter under '/product' path
+
+
+// Connect to MongoDB
 mongoose.connect('mongodb://127.0.0.1:27017/Shop', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Error connecting to MongoDB:', err));
 
-app.use(bodyParser.json());
-
-app.use(session({
-  secret: 'strong-secret-key',
-  genid: function () {
-    return crypto.randomBytes(16).toString('hex');
-  },
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    secure: false,
-    httpOnly: true,
-    maxAge: 1000 * 60 * 60,
-  },
-}));
-
-// Routes
-app.post('/register', userService.registerUser);
-app.post('/login',userService.loginUser); // Add login route
-app.post('/addProduct', productService.addProduct);
-app.put('/products/update/:Id', productService.updateProduct);
-app.delete('/products/delete/:Id', productService.deleteProduct);
-app.get('/products', productService.getAllProducts);
-
-app.listen(3001, () => {
-  console.log(`Server is running on port 3001`);
+// Start the server
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
