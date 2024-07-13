84c0a02733367a7bcf49f3f7f32e6d9eb7f1e2ca
Poorna Malinhara Kuruppu Arachchi <131478375+Malinhara@users.noreply.github.com>
2024-07-13 17:10:31 +0530

    Update controller.js

--- Backend/controller.js
+++ Backend/controller.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const session = require('express-session');
 const bodyParser = require('body-parser');
-const crypto = require('crypto');
 const cors = require('cors');
 const userService = require('./Service/User service');
 const productService = require('./Service/product service');
-
+const PORT = 3001;
+const HOST = '127.0.0.1'; // Define your host here
 const app = express();
 
 // Middleware
@@ -15,11 +14,11 @@ app.use(express.json());
 app.use(bodyParser.json());
 
 
-
 // Routes
 const userRouter = express.Router();
 userRouter.post('/register', userService.registerUser);
 userRouter.post('/login', userService.loginUser);
+userRouter.post('/checkcode', userService.checkCode);
 userRouter.put('/buyProduct', userService.userBuy);
 userRouter.post('/findproduct', userService.findproduct);
 app.post('/logout', userService.logoutUser);
@@ -35,12 +34,13 @@ app.use('/product', productRouter); // Mount productRouter under '/product' path
 
 
 // Connect to MongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/Shop', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('Error connecting to MongoDB:', err));
+const uri = "mongodb+srv://poornainvest0:123456mp@app.mvyhzcz.mongodb.net/userdb?retryWrites=true&w=majority";
+mongoose.connect(uri, {
+})
+  .then(() => console.log('Connected to MongoDB Atlas'))
+  .catch(err => console.error('Error connecting to MongoDB Atlas:', err));
 
 // Start the server
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, HOST, () => {
+  console.log(`Server is running on http://${HOST}:${PORT}`);
 });
